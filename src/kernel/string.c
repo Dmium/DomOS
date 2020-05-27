7c41a1c8a9b656bb2eaa9e522c4408d2d05037fe
Dom Hall <dominiczy.hall@gmail.com>
2020-05-27 15:24:16 +0100

    Fixed strcmp issues found by unit tests

--- src/kernel/string.c
+++ src/kernel/string.c
@@ -1,5 +1,5 @@
 #include <stddef.h>
-
+#include "kernel.h"
 size_t strlen(char* str) {
     size_t len = 0;
     while(str[len] != 0) {
@@ -10,11 +10,18 @@ size_t strlen(char* str) {
 
 //TODO proper return values (currently just true or false)
 int strcmp(char* str1, char* str2) {
-    size_t len = strlen(str1);
+    size_t len1 = strlen(str1);
+    size_t len2 = strlen(str2);
     size_t pos = 0;
-    while(str1[pos] == str2[pos]) {
-        pos++;
+    if (len1 == len2) {
+        while(pos < len1 && str1[pos] == str2[pos]) {
+            pos++;
+        }
+        return (len1 == pos);
     }
-    return (len + 1 == pos);
+    return 0;
 
 }
+
+// void strcat(char* str1, char* str2) {
+// }
